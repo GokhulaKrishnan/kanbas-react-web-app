88213783cd70620c30e2fe7e0b398399ba0c4cea
Gokhulgetz <gokhulakrishnant@gmail.com>
2024-09-19 18:20:12 -0400

    Done with reportWebVitals

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { HashRouter } from "react-router-dom";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Labs from "./labs";
--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,14 +1,14 @@
-import { ReportHandler } from 'web-vitals';
+import { onCLS, onINP, onFCP, onLCP, onTTFB } from "web-vitals";
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+type OnPerfEntry = (metric: any) => void; // Use a more general type for the callback
+
+const reportWebVitals = (onPerfEntry?: OnPerfEntry) => {
+  if (onPerfEntry && typeof onPerfEntry === "function") {
+    onCLS(onPerfEntry);
+    onINP(onPerfEntry);
+    onFCP(onPerfEntry);
+    onLCP(onPerfEntry);
+    onTTFB(onPerfEntry);
   }
 };
 
